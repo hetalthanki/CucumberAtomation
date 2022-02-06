24d69005448c5f285d7e434f2ee4a7e840c3d686
hetalthanki <hetalthanki52@gmail.com>
2022-02-06 12:34:09 +0000

    creating a framework

---
+++ src/test/java/Hooks.java
@@ -0,0 +1,17 @@
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
+public class Hooks
+{
+    @Before
+    public void beforeTest()
+    {
+
+    }
+
+    @After
+    public  void afterTest()
+    {
+
+    }
+}
---
+++ src/test/java/Run.java
@@ -0,0 +1,10 @@
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)   //all .java file converted to .class file (compilation file) that files are used by cucumber
+@CucumberOptions(features = ".",tags = "")
+public class Run
+{
+
+}
